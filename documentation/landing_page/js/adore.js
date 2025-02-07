f4f44cced853fe850aaf3b2de4f293a6083c72a9
Niklas <niklas@die-4-boegers.de>
2025-02-07 11:46:09 +0100

    combined landing page into index.html, deleted remaining files

--- documentation/landing_page/js/adore.js
+++ documentation/landing_page/js/adore.js
@@ -2,5 +2,16 @@ new ClipboardJS('.btn');
 
 $(document)
 $(document).ready(function() {
+  // Initialize tooltips
   $('[data-toggle="tooltip"]').tooltip({show: null});
 });
+
+function show(shown) {
+  document.getElementById('about').style.display='none';
+  document.getElementById('getting-started').style.display='none';
+  document.getElementById('contribute').style.display='none';
+  document.getElementById('sponsors').style.display='none';
+  document.getElementById('contact').style.display='none';
+  document.getElementById(shown).style.display='block';
+  return false;
+}
