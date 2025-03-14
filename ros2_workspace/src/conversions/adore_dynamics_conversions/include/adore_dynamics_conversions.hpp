aea324c914e79c29cd0b2f6b7fe30c058d26473c
n-mat <matthias.nichting@dlr.de>
2025-03-14 03:45:45 -0700

    fix of includes

--- ros2_workspace/src/conversions/adore_dynamics_conversions/include/adore_dynamics_conversions.hpp
+++ ros2_workspace/src/conversions/adore_dynamics_conversions/include/adore_dynamics_conversions.hpp
@@ -32,6 +32,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/utils.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 namespace adore
 {
